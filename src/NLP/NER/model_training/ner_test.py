18ab7ff6118dc7ab3bda8302d1d6052d356425f0
Arkaqius <kuleta.arkadiusz@gmail.com>
2023-09-05 00:21:58 +0200

    add better test suite

--- src/NLP/NER/model_training/ner_test.py
+++ src/NLP/NER/model_training/ner_test.py
@@ -5,28 +5,33 @@ import random
 from vh_ner import VhNer, VhProcessedText
 import nlp.ner.config as cfg
 
-
 def main():
     '''
     Main function to perform NER model test
     '''
     ner = VhNer(cfg.PATH_TRAINED_MODEL)
 
-    with open(cfg.PATH_TEST_SENTENCES, "r", encoding='UTF-8') as file:
-        lines = file.readlines()
-        for _ in range(1, 50):
-            random_line = random.choice(lines)
-            print("\n" + random_line)
+    while True:
+        user_input = input("Enter an utterance or type 'random' for random test sentence, or 'exit' to stop: ")
 
-            result:  VhProcessedText = ner.process_text(random_line)
-            print("Named entities:")
-            for entity in result.named_entities:
-                print(
-                    f"{entity.entity_type}: {entity.entity_default_name} : {entity.entity_text}")
+        if user_input.lower() == 'exit':
+            break
+        elif user_input.lower() == 'random':
+            with open(cfg.PATH_TEST_SENTENCES, "r", encoding='UTF-8') as file:
+                lines = file.readlines()
+                user_input = random.choice(lines)
+                print("\nSelected random line:", user_input)
+        
+        result: VhProcessedText = ner.process_text(user_input)
 
-            print("Numerical values:")
-            for num_value in result.numerical_values:
-                print(f"{num_value.value} {num_value.unit}")
+        # Displaying results
+        print("Named entities:")
+        for entity in result.named_entities:
+            print(f"{entity.entity_type}: {entity.entity_default_name} : {entity.entity_text}")
+
+        print("Numerical values:")
+        for num_value in result.numerical_values:
+            print(f"{num_value.value} {num_value.unit}")
 
 
 if __name__ == "__main__":
